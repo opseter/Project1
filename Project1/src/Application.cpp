23a8b3af8fbb78c27b892e36f4403740b696cab6
opseter <opseter@outlook.com>
2020-08-14 23:27:56 +0800

    fix imgui

--- Project1/src/Application.cpp
+++ Project1/src/Application.cpp
@@ -102,12 +102,7 @@ int main(void)
         //difine view's size
 
         //glm::mat4 proj = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-
-        glm::mat4 mvp = proj * view * model;
+        
         
 
         Shader shader("res/shaders/Test2.shader");
@@ -116,8 +111,11 @@ int main(void)
         texture.Bind();
         shader.SetUniform1i("texture1", 0);
 
+        glm::vec3 translation(200, 200, 0);
 
-        shader.SetUniformMat4f("u_MVP", mvp);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        
         //unbind everything 
        
         va.Unbind();
@@ -137,10 +135,6 @@ int main(void)
             /* Render here */
             renderer.Clear();
 
-            //va.Bind();
-            //vb.Bind();
-            //ib.Bind();
-            //shader.Bind();
             renderer.Draw(va, ib, shader);
            
 
@@ -149,59 +143,31 @@ int main(void)
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-            if (show_demo_window)
-                ImGui::ShowDemoWindow(&show_demo_window);
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+            shader.SetUniformMat4f("u_MVP", mvp);
+         
 
-            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+           
             {
-                static float f = 0.0f;
-                static int counter = 0;
-
-                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
+               
+                ImGui::Begin("Hello, world!");                                
+                ImGui::SliderFloat3("float", &translation.x, 0.0f, 960.0f);           
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
                 ImGui::End();
             }
 
-            // 3. Show another simple window.
-            if (show_another_window)
-            {
-                ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-                ImGui::Text("Hello from another window!");
-                if (ImGui::Button("Close Me"))
-                    show_another_window = false;
-                ImGui::End();
-            }
+           
 
             // Rendering
             ImGui::Render();
-            int display_w, display_h;
+           /* int display_w, display_h;
             glfwGetFramebufferSize(window, &display_w, &display_h);
             glViewport(0, 0, display_w, display_h);
             glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);*/
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-
-            
-
-
-
-            //glClearColor(0.0f, 0.5f, 0.0f, 1.0f);//set the color which will clear the scream
-            //glClear(GL_COLOR_BUFFER_BIT);//apply the color prior setted
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
